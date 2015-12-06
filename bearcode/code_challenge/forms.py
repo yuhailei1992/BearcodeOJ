f95c719852c1d351d2fe4caeb18042048a8afce1
yuhailei1992 <haileiy@andrew.cmu.edu>
2015-12-06 00:58:08 -0500

    Clean up forms.py.

--- bearcode/code_challenge/forms.py
+++ bearcode/code_challenge/forms.py
@@ -1,23 +1,22 @@
 from django import forms
-from django.core.files.images import get_image_dimensions
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import PasswordResetForm
+from django.forms import Textarea
+
 from models import *
-from django.forms import ModelForm, Textarea
+
 
 class RegistrationForm(forms.Form):
     username = forms.EmailField()
     email = forms.EmailField()
-    password1 = forms.CharField(max_length = 200, 
-                                label='Password', 
-                                widget = forms.PasswordInput())
-    password2 = forms.CharField(max_length = 200, 
-                                label='Confirm password',  
-                                widget = forms.PasswordInput())
-    firstname = forms.CharField(max_length = 200,
+    password1 = forms.CharField(max_length=200,
+                                label='Password',
+                                widget=forms.PasswordInput())
+    password2 = forms.CharField(max_length=200,
+                                label='Confirm password',
+                                widget=forms.PasswordInput())
+    firstname = forms.CharField(max_length=200,
                                 label='firstname')
-    lastname = forms.CharField(max_length = 200,
-                                label='lastname')
+    lastname = forms.CharField(max_length=200,
+                               label='lastname')
 
     # Customizes form validation for properties that apply to more
     # than one field.  Overrides the forms.Form.clean function.
@@ -35,32 +34,28 @@ class RegistrationForm(forms.Form):
         # Generally return the cleaned data we got from our parent.
         return cleaned_data
 
-
     # Customizes form validation for the username field.
     def clean_username(self):
-        # Confirms that the username is not already present in the
-        # User model database.
+        # Confirms that the username is not already present in the user model database.
         username = self.cleaned_data.get('username')
         if User.objects.filter(username__exact=username):
             raise forms.ValidationError("This email address has already been registered.")
 
-        # Generally return the cleaned data we got from the cleaned_data
-        # dictionary
+        # Generally return the cleaned data we got from the cleaned_data dictionary.
         return username
 
-#class ImageUploadForm(forms.Form):
-#   """Image upload form."""
-#  image = forms.ImageField()
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user', 'username', 'following', 'ranking_score', 'success_rate','role', 'image')
+        exclude = (
+            'user', 'username', 'following', 'ranking_score', 'success_rate', 'role', 'image')
         widgets = {
             'description': Textarea(attrs={'cols': 80, 'rows': 50}),
         }
         field = ('firstname', 'lastname', 'age', 'bio')
-    
+
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -68,38 +63,37 @@ class PostForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super(PostForm, self).clean()
-
-        text = cleaned_data.get('posttext')
-        #if not text:
-        #    raise forms.ValidationError("You must input something to send")
-
         return cleaned_data
+
 
 class ProblemForm(forms.ModelForm):
     class Meta:
         model = Problem
         exclude = {'visible', 'success_rate'}
-        field = ('name', 'description', 'example', 'difficulty', 'java_default','python_default', 'tle_limit', 'mle_limit','java_tests','python_tests')
+        field = ('name', 'description', 'example', 'difficulty', 'java_default', 'python_default',
+                 'tle_limit', 'mle_limit', 'java_tests', 'python_tests')
+
 
 class DiscussionForm(forms.Form):
-
     discussiontitle = forms.CharField(max_length=50)
     discussiontext = forms.CharField(max_length=1000)
 
     def clean(self):
-        cleaned_data = super(DiscussionForm,self).clean()
+        cleaned_data = super(DiscussionForm, self).clean()
         return cleaned_data
+
 
 class CommentForm(forms.Form):
     commenttext = forms.CharField(max_length=100)
 
     def clean(self):
-        cleaned_data = super(CommentForm,self).clean()
+        cleaned_data = super(CommentForm, self).clean()
         return cleaned_data
+
 
 class HistoryForm(forms.Form):
     codecontent = forms.CharField(max_length=10000)
 
     def clean(self):
-        cleaned_data = super(HistoryForm,self).clean()
+        cleaned_data = super(HistoryForm, self).clean()
         return cleaned_data
