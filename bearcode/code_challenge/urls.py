55f54f00a302c55bf6b10026f95236bed79c6858
yuhailei1992 <haileiy@andrew.cmu.edu>
2015-12-06 01:19:34 -0500

    Split views_profile.

--- bearcode/code_challenge/urls.py
+++ bearcode/code_challenge/urls.py
@@ -53,12 +53,11 @@ urlpatterns = [
     url(r'^login$', 'django.contrib.auth.views.login',
         {'template_name': 'code_challenge/login.html'}, name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
-    url(r'^register$', 'code_challenge.views.register', name='regitser'),
+    url(r'^register$', 'code_challenge.views_profile.register', name='regitser'),
 
     # General.
     url(r'^$', 'code_challenge.views.home', name='home'),
     url(r'^code_challenge$', 'code_challenge.views.home', name='home'),
-    # url(r'^add-post', 'code_challenge.views.add_post', name='add'),
     url(r'^problem/(?P<problemid>\d+)/$', 'code_challenge.views.problem', name='problem'),
 
     # Access control.
@@ -71,8 +70,8 @@ urlpatterns = [
 
     # User Profile.
     url(r'^profile/(?P<username>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})$',
-        'code_challenge.views.profile', name='profile'),
-    url(r'^edit_profile$', 'code_challenge.views.edit_profile', name='edit'),
+        'code_challenge.views_profile.profile', name='profile'),
+    url(r'^edit_profile$', 'code_challenge.views_profile.edit_profile', name='edit'),
     url(r'^change_password$', 'django.contrib.auth.views.password_change',
         {'template_name': 'code_challenge/password_change_form.html'}, name='password_change'),
     url(r'^change_password/done/$', 'django.contrib.auth.views.password_change_done',
